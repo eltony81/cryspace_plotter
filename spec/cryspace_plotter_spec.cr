4edf5acb192f5a7ee34d715b57ec9a7b2809527d
tony <antonio.difluri@gmail.com>
2026-06-06 07:56:11 +0200

    Replace placeholder spec with a valid integration test

--- spec/cryspace_plotter_spec.cr
+++ spec/cryspace_plotter_spec.cr
@@ -1,9 +1,24 @@
 require "./spec_helper"
+require "cryspace"
 
 describe CryspacePlotter do
-  # TODO: Write tests
+  it "successfully simulates the closed-loop RLC system with PID" do
+    r, l, c = 1.0, 0.5, 0.1
+    a = [[0.0, 1/c], [-1/l, -r/l]].to_tensor
+    b = [[0.0], [1/l]].to_tensor
+    c = [[1.0, 0.0]].to_tensor
+    d = [[0.0]].to_tensor
+    rlc_plant = CrySpace::StateSpace.new(a, b, c, d)
 
-  it "works" do
-    false.should eq(true)
+    kp, ki, kd = 3.0, 5.0, 1.5
+    tf = 0.01
+    pid_num = [(kp*tf + kd), (kp + ki*tf), ki].to_tensor
+    pid_den = [tf, 1.0, 0.0].to_tensor
+    pid_controller = CrySpace::TransferFunction.new(pid_num, pid_den).to_statespace
+
+    sys_cl = (rlc_plant * pid_controller).feedback([[1.0]].to_tensor)
+    sys_cl.should_not be_nil
+    sys_cl.n_states.should eq(4)
   end
 end
+
